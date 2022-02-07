aec908e151d53eb83564365af492c5395f7ef09d
takenokonyokki <97602717+takenokonyokki@users.noreply.github.com>
2022-02-07 15:20:13 +0000

    [Finish]3-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,20 +1,17 @@
 class BooksController < ApplicationController
-  def new
+  def index
+    @books = Book.all
     @book = Book.new
   end
 
   def create
     @book = Book.new(book_params)
     if @book.save
-      redirect_to book_path(@book.id)
+      redirect_to book_path(@book.id), notice:'Book was successfully created.'
     else
-      render :'/books'
+      @books = Book.all
+      render :index
     end
-  end
-
-  def index
-    @books = Book.all
-    @book = Book.new
   end
 
   def show
@@ -26,9 +23,12 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id), notice:'Book was successfully created.'
+    else
+      render :edit
+    end
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
   get '/books' => 'books#index'
-  get '/books' => 'books#new'
   post '/books' => 'books#create'
   get '/books/:id' => 'books#show', as:'book'
   get '/books/:id/edit' => 'books#edit', as:'edit_book'
